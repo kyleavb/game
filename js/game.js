9c3d4e6d3cfccf1f30d9a057ca251df434110915
Kyle Van Bergen <kyleavb@gmail.com>
2018-02-06 22:13:32 -0800

    gravity, friction, acceleration added to players, refactored code to reduce redundency

--- js/game.js
+++ js/game.js
@@ -8,17 +8,14 @@ var monster = document.getElementById("monster");
 var ctxMonster = monster.getContext("2d");
 var ui = document.getElementById("ui");
 var ctxUi = ui.getContext("2d");
-background.width = $(window).innerWidth();
-play1.width = $(window).innerWidth();
-play2.width = $(window).innerWidth();
-monster.width = $(window).innerWidth();
-ui.width = $(window).innerWidth();
-background.height = $(window).innerHeight();
-play1.height = $(window).innerHeight();
-play2.height = $(window).innerHeight();;
-monster.height = $(window).innerHeight();;
-ui.height = $(window).innerHeight();
+var canvas = $(".game");
+for(var i=0;i<canvas.length;i++){
+  canvas[i].width = $(window).innerWidth();
+  canvas[i].height = $(window).innerHeight();
+}
 var landable = [];
+var gravity = .5;
+var friction = 0.97;
 
 //Image Loader
 //----------------Player1 Image -----------------------------
@@ -53,51 +50,47 @@ groundTile.src = "img/ground/Tile_11.png"
 
 //ground object constructor
 function groundObject(options){
-  var that = {};
-  that.yStart = options.yStart,
-  that.xStart = options.xStart,
-  that.yEnd = options.yEnd,
-  that.xEnd = options.xEnd
-
-  return that;
+  var ret = {};
+  ret.xStart = options.xStart;
+  ret.xEnd = options.xEnd;
+  ret.yStart = options.yStart;
+  ret.yEnd = options.yEnd;
+  ret.img = options.img;
+  return ret;
 }
 //sprite constructor
 function sprite(options){
   var that = {};
-  that.x = options.x,
-  that.y = options.y,
-  that.speedX = 0,
-  that.speedY = 0,
-  that.gravity = .1,
-  that.gravitySpeed = 0,
+  that.x = options.x;
+  that.y = options.y;
+  that.vX = 0;
+  that.vY = 0;
+  that.move = false;
   that.onGround = options.onGround,
+  that.isJumping = false,
+  that.life = 3,
+  that.facing = "right";
   frameIndex = 0,
   that.tickCount = 0,
-  that.move = options.move,
-  that.moveSpeed = options.moveSpeed,
   that.ticksPerFrame = options.ticksPerFrame,
   that.numberOfFrames = options.numberOfFrames,
   that.loop = options.loop,
   that.context = options.context,
-  that.width = options.width,
-  that.height = options.height,
-  that.image = options.image,
+  that.imgWidth = options.imgWidth,
+  that.imgHeight = options.imgHeight,
+  that.img = options.img,
 
   that.render = function(){
-    that.context.clearRect(that.x,that.y, that.width, that.height);
-    if(that.move){
-      that.x += that.moveSpeed;
-    }
     that.context.drawImage(
-      that.image,
+      that.img,
       0,//Source Start X
-      frameIndex * that.height / that.numberOfFrames, // Source Start Y
-      that.width,//Soruce xSize
-      that.height / that.numberOfFrames,//sorce ySize height
+      frameIndex * that.imgHeight / that.numberOfFrames, // Source Start Y
+      that.imgWidth,//Soruce xSize
+      that.imgHeight / that.numberOfFrames,//sorce ySize height
       that.x,
       that.y,
-      that.width,
-      that.height / that.numberOfFrames);
+      that.imgWidth,
+      that.imgHeight / that.numberOfFrames);
   },
 
   that.update = function(){
@@ -117,44 +110,58 @@ function sprite(options){
 
 function updatePosition(obj){
   for(var i=0; i<landable.length; i++){
-    if(obj.x > landable[i].xStart && obj.x < landable[i].xEnd && obj.y > landable[i].yStart && obj.y < landable[i].yEnd){
+    if(obj.x > landable[i].xStart && obj.x < landable[i].xEnd && obj.y > landable[i].yEnd){
       obj.onGround = true;
+      obj.vY = 0;
     }
   }
-  //gravity force
+  if(obj.move){
+    if(obj.facing === "right"){
+      obj.vX += .5;
+    }else{
+      obj.vX -= .5;
+    }
+    obj.vY += 0;
+    obj.vX *= friction;
+    obj.vY *= friction;
+    obj.x += obj.vX;
+    obj.y += obj.vY;
+  }
+  //Falling
   if(!obj.onGround){
-    obj.gravitySpeed += obj.gravity;
-    obj.x += obj.speedX;
-    obj.y += obj.speedY + obj.gravitySpeed;
+    obj.vY += gravity;
+    obj.y += obj.vY;
   }
 }
 
-function drawGround(){
+function createGround(){
   var numOfTile = background.width / 256;
   var tilePos = 0;
   for(var i=0; i<= numOfTile; i++){
-    ctxBack.drawImage(groundTile, tilePos, background.height - 200);
+    var rock = groundObject({
+      xStart:tilePos,
+      xEnd: tilePos+groundTile.width,
+      yStart: background.height - 225,
+      yEnd: background.height - 200,
+      img: groundTile
+    })
+    landable.push(rock);
     tilePos += 255;
   }
-  var floor = groundObject({
-    yStart: 0,
-    xStart: background.height - 200,
-    yEnd: tilePos,
-    xEnd: background.height - 200
-  })
-  landable.push(floor);
 }
 
-function gameInit(){
-  //Load splash screen
-  // user Inputs to check game type
-
+function updateGround(){
+  //nove griound when needed
 }
 
 function gameLoop(){
-  player1.context.clearRect(player1.x,player1.y, player1.width, player1.height);
+  landable.forEach(function(item){
+    updateGround(item);
+    ctxBack.drawImage(item.img, item.xStart, item.yStart)
+  });
+  player1.context.clearRect(player1.x,player1.y, player1.imgWidth, player1.imgHeight);
   player1.update();
-  player2.context.clearRect(player2.x,player1.y, player2.width, player2.height);
+  player2.context.clearRect(player2.x,player1.y, player2.imgWidth, player2.imgHeight);
   player2.update();
   player1.render();
   player2.render();
@@ -163,115 +170,113 @@ function gameLoop(){
 
 var player1 = sprite({
   x: 30,
-  y: 0,
+  y: 30,
   context: ctxPlay1,
-  width: 46,
-  height: 879,
-  image: player1IdleRight,
+  imgWidth: 46,
+  imgHeight: 879,
+  img: player1IdleRight,
   numberOfFrames: 10,
   ticksPerFrame: 4,
   loop: true,
   facing: "right",
-  onGround: false
+  onGround: false,
+  move: false
 });
 
 var player2 = sprite({
   x: 30,
-  y: 0,
+  y: 30,
   context: ctxPlay2,
-  width: 58,
-  height: 1000,
-  image: player2IdleRight,
+  imgWidth: 58,
+  imgHeight: 1000,
+  img: player2IdleRight,
   numberOfFrames: 10,
   ticksPerFrame: 20,
   loop: true,
   facing: "right",
-  onGround: false
+  onGround: false,
+  move: false
 });
-
 //When finished loading last image, run gameLoop
 player1RunLeft.onload = function(){
   player2.render();
   player1.render();
-  drawGround();
+  createGround();
   gameLoop();
 }
 
 //KeyInput function
 $(document).keydown(function(e){
   if(e.keyCode === 39){//move right
-    player1.image = player1RunRight;
+    player1.img = player1RunRight;
     player1.facing = "right";
     player1.loop = true;
-    player1.ticksPerFrame = 1;
-    player1.width = 72;
-    player1.height = 917;
-    player1.move = true;
-    player1.moveSpeed = 5;
+    player1.ticksPerFrame = 4;
+    player1.imgWidth = 72;
+    player1.imgHeight = 917;
   }
   if(e.keyCode === 37){//move left
-    player1.context.clearRect(player1.x,player1.y, player1.width, player1.height);
-    player1.image = player1RunLeft;
+    player1.img = player1RunLeft;
     player1.facing = "left";
     player1.loop = true;
-    player1.ticksPerFrame = 1;
-    player1.width = 72;
-    player1.height = 917;
+    player1.ticksPerFrame = 4;
+    player1.imgWidth = 72;
+    player1.imgHeight = 917;
     player1.move = true;
     player1.moveSpeed = -5;
   }
   if(e.keyCode === 32){ //Attack
-    player1.context.clearRect(player1.x,player1.y, player1.width, player1.height);
     if(player1.facing === "right"){
-      player1.image = player1AttackRight;
+      player1.img = player1AttackRight;
     }else{
-      player1.image = player1AttackLeft;
+      player1.img = player1AttackLeft;
     }
     player1.loop = true;
-    player1.ticksPerFrame = 2;
-    player1.width = 107;
-    player1.height = 991;
+    player1.ticksPerFrame = 4;
+    player1.imgWidth = 107;
+    player1.imgHeight = 991;
     player1.move = false;
     player1.moveSpeed = 0;
   }
   if(e.keyCode === 38){//up key
-
+    player1.loop = false;
+    player1.ticksPerFrame = 4;
+    player1.imgWidth = 107;
+    player1.imgHeight = 991;
+    player1.isJumping = true;
   }
   if(e.keyCode === 40){//Down key
 
   }
   if(e.keyCode === 68){//right D key
-    player2.context.clearRect(player2.x,player1.y, player2.width, player2.height);
-    player2.image = player2RunRight;
+    player2.img = player2RunRight;
     player2.facing = "right";
     player2.loop = true;
-    player2.ticksPerFrame = 1;
-    player2.width = 75;
-    player2.height = 1041;
+    player2.ticksPerFrame = 4;
+    player2.imgWidth = 75;
+    player2.imgHeight = 1041;
     player2.move = true;
     player2.moveSpeed = 5;
   }
   if(e.keyCode === 65){//Left A key
-    player2.context.clearRect(player2.x,player1.y, player2.width, player2.height);
-    player2.image = player2RunLeft;
+    player2.img = player2RunLeft;
     player2.facing = "left";
     player2.loop = true;
-    player2.ticksPerFrame = 1;
-    player2.width = 75;
-    player2.height = 1041;
+    player2.ticksPerFrame = 4;
+    player2.imgWidth = 75;
+    player2.imgHeight = 1041;
     player2.move = true;
-    player2.moveSpeed = -5;
   }
   if(e.keyCode === 70){
     if(player2.facing === "right"){
-      player2.image = player2AttackRight;
+      player2.img = player2AttackRight;
     }else{
-      player2.image = player2AttackLeft;
+      player2.img = player2AttackLeft;
     }
     player2.loop = true;
-    player2.ticksPerFrame = 2;
-    player2.width = 104;
-    player2.height = 1137;
+    player2.ticksPerFrame = 4;
+    player2.imgWidth = 104;
+    player2.imgHeight = 1137;
     player2.move = false;
     player2.moveSpeed = 0;
   }
@@ -279,32 +284,32 @@ $(document).keydown(function(e){
 });
 
 $(document).keyup(function(e){
+  player1.context.clearRect(player1.x,player1.y, player1.imgWidth, player1.imgHeight);
+  player2.context.clearRect(player2.x,player1.y, player2.imgWidth, player2.imgHeight);
   if(e.keyCode === 40 || e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 39 || e.keyCode === 32){
-    player1.context.clearRect(player1.x,player1.y, player1.width, player1.height);
     if (player1.facing === "right") {
-      player1.image = player1IdleRight;
+      player1.img = player1IdleRight;
     }else{
-      player1.image = player1IdleLeft;
+      player1.img = player1IdleLeft;
     }
     player1.loop = true;
     player1.ticksPerFrame = 4;
-    player1.width = 46;
-    player1.height = 879;
+    player1.imgWidth = 46;
+    player1.imgHeight = 879;
     player1.move = false
-    player1.moveSpeed = 0;
+    //player1.vX = 0;
   }
   if(e.keyCode === 87 || e.keyCode === 65 || e.keyCode === 83 || e.keyCode === 68 || e.keyCode === 70){
-    player2.context.clearRect(player2.x,player2.y, player2.width, player2.height);
     if (player2.facing === "right") {
-      player2.image = player2IdleRight;
+      player2.img = player2IdleRight;
     }else{
-      player2.image = player2IdleLeft;
+      player2.img = player2IdleLeft;
     }
     player2.loop = true;
     player2.ticksPerFrame = 4;
-    player2.width = 58;
-    player2.height = 1001;
+    player2.imgWidth = 58;
+    player2.imgHeight = 1001;
     player2.move = false
-    player2.moveSpeed = 0;
+    //dplayer2.vX = 0;
   }
 });
